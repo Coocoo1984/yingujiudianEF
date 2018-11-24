837400c139e3a8d9e5a96187ee83a64e7d57d84c
Coocoo1984 <yan.coocoo@qq.com>
2018-11-25 06:47:05 +0800

    从项目中暂时排除Role 更新Usr

---
+++ PurocumentLib/Message/Handler/UpdateUsrRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DevelopBase.Message;
+using DevelopBase.Common;
+using PurocumentLib.Model;
+using PurocumentLib.Service;
+using PurocumentLib.Message.Request;
+namespace PurocumentLib.Message.Handler
+{
+    public class UpdateUsrHandler : HandlerGeneric<UpdateUsrRequest>
+    {
+        public UpdateUsrHandler(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public override ResponseBase Handler(UpdateUsrRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (string.IsNullOrEmpty(request.Name))
+            {
+                throw new Exception("用户名称无效");
+            }
+            var model = new Model.Usr()
+            {
+                WechatID = request.WechatID,
+                Code = request.Code,
+                Name = request.Name,
+                Addr = request.Addr,
+                Addr1 = request.Addr1,
+                Tel = request.Tel,
+                Tel1 = request.Tel1,
+                Mobile = request.Mobile,
+                Mobile1 = request.Mobile1
+            };
+            var service = ServiceProvider.GetService<IUsrService>();
+            service.Update(model);
+            return new ResponseBase() { Result = 1, ResultInfo = "" };
+        }
+    }
+}
---
+++ PurocumentLib/Service/RoleService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using DevelopBase.Common;
+using DevelopBase.Services;
+using PurocumentLib.Dbcontext;
+using PurocumentLib.Entity;
+using PurocumentLib.Model;
+
+namespace PurocumentLib.Service
+{
+    public class RoleService : ServiceBase, IRoleService
+    {
+        public RoleService(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public void Add(Model.Role model)
+        {
+            if (string.IsNullOrEmpty(model.Name))
+            {
+                throw new Exception("角色名称无效");
+            }
+            var dbContext = ServiceProvider.GetDbcontext<IPurocumentDbcontext>();
+            if (string.IsNullOrEmpty(model.Code))
+            {
+                throw new Exception("角色编码无效");
+            }
+            if (dbContext.Role.Count(c => c.Code == model.Code) > 0)
+            {
+                throw new Exception("角色编码无效");
+            }
+            var entity = new Entity.Role()
+            {
+                WechatGroupID = model.WechatGroupID,
+                Code = model.Code,
+                Name = model.Name
+            };
+            dbContext.Add(entity);
+            dbContext.SaveChanges();
+        }
+
+        public Model.Role Load(int id)
+        {
+            var dbContext = ServiceProvider.GetDbcontext<IPurocumentDbcontext>();
+            var entity = dbContext.Department.SingleOrDefault(s => s.ID == id);
+            if (entity == null)
+            {
+                return null;
+            }
+            var model = new Model.Role()
+            {
+                ID = entity.ID,
+                WechatGroupID = entity.WechatID,
+                Name = entity.Name,
+                Code = entity.Code
+            };
+            return model;
+        }
+
+        public void Update(Model.Role model)
+        {
+            var dbContext = ServiceProvider.GetDbcontext<IPurocumentDbcontext>();
+            var entity = dbContext.Role.SingleOrDefault(s => s.ID == model.ID);
+            if (entity == null)
+            {
+                throw new Exception("角色信息不存在");
+            }
+
+            if (string.IsNullOrEmpty(model.Name))
+            {
+                throw new Exception("角色名称无效");
+            }
+
+            if (string.IsNullOrEmpty(model.Code))
+            {
+                throw new Exception("角色编码无效");
+            }
+            if (dbContext.Department.Count(c => c.Code == model.Code && c.ID != model.ID) > 0)
+            {
+                throw new Exception("角色编码无效");
+            }
+            entity.Code = model.Code;
+            entity.WechatGroupID = model.WechatGroupID;
+            entity.Name = model.Name;
+            dbContext.Update(entity);
+            dbContext.SaveChanges();
+        }
+
+        public bool ValidateRoleID(IEnumerable<int> roleIDs)
+        {
+            var dbContext = ServiceProvider.GetDbcontext<IPurocumentDbcontext>();
+            if (dbContext.Department.Count(c => roleIDs.Contains(c.ID)) == roleIDs.Count())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
